df0920fd3e6901ab0a75036a34550b6c34bc389c
Kong Youngin <kongyi123@nate.com>
2020-02-02 08:15:42 +0900

    leetcode number 2 translation to korean

---
+++ HelloWorld/HelloWorld/76.cpp
@@ -0,0 +1,8 @@
+// 76. Minimum Window Substring
+/*
+	string S와 T가 주어졌을 때, T의 모든 문자들을 포함하는 minimum window S를 구하여라. 
+	시간복잡도 O(n) 만에.
+
+	- T의 모든 문자들을 커버하는 window S가 없다면, 빈 string인 ""를 return 하라.
+	- 주어진 데이터는 window S가 유일하게 존재하거나 존재하지 않는 것만 제공된다.
+*/
