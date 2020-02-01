61450a7fe94ceda7e8772f76720e2702115dcf09
Kong Youngin <kongyi123@nate.com>
2020-02-02 08:24:37 +0900

    leetcode number 1 problem translation to korean

---
+++ HelloWorld/HelloWorld/124.cpp
@@ -0,0 +1,10 @@
+// 124. Binary Tree Maximum Path Sum
+/*
+
+non-empty binary tree가 주어질 때, maximum path sum을 구하여라.
+	이 문제에서 path란 x 노드에서 y 노드 까지의 경로를 의미한다. (x,y 는 트리상의 any 노드)
+	
+	입력 : 자식을 향하는 주소가 두 개 있는 노드들로 구성된 이진 트리의 root
+	출력 :int (path에 있는 노드의 value들의 sum의 최대)
+
+*/
