dcf045046a4e89ebeac05cbd591558cc251f8a50
Kong Youngin <kongyi123@nate.com>
2020-02-29 22:17:18 +0900

    Longest Consecutive Sequence - LeetCode 3 sol

--- hello2/hello2/128_sol.cpp
+++ hello2/hello2/128_sol.cpp
@@ -1,10 +1,13 @@
-// sort O(NlogN)
+// hash O(N)
 // 코드 최적화 x
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <vector>
-#include <map>
+//#include <map>
+#include <unordered_map> // hash map
 
+// map : treeMap
+// unordered_map : hashMap
 using namespace std;
 
 int max(int a, int b) {
@@ -15,22 +18,33 @@ int max(int a, int b) {
 class Solution {
 public:
 	int longestConsecutive(vector<int>& nums) {
-		map<int, int> map;
+
+		unordered_map<int, int> map;
+		int maxVal = 0;
 		for (auto n : nums) {
-			int val = map.find(n);
+			if (!map[n]) {
+				int left = map[n - 1] ? map[n - 1] : 0;
+				int right = map[n + 1] ? map[n + 1] : 0;
+				int sum = left + right + 1;
+
+				map[n] = sum;
+				maxVal = max(maxVal, sum);
+
+				map[n - left] = sum;
+				map[n + right] = sum;
+			}
 		}
-		
+
 		return maxVal;
 	}
 };
-
 
 
 int main(void) {
 	vector<int> nums;
 
 	nums.push_back(1);
-	/*	nums.push_back(1);
+		nums.push_back(1);
 		nums.push_back(4);
 		nums.push_back(7);
 		nums.push_back(3);
@@ -40,7 +54,7 @@ int main(void) {
 		nums.push_back(8);
 		nums.push_back(-1);
 		nums.push_back(6);
-	*/
+	
 	Solution sol;
 	int result = sol.longestConsecutive(nums);
 	printf("max length = %d", result);
