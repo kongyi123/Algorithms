f15a6d7bcfa92fdd07373b3a62c0a353d0b52232
Kong Youngin <kongyi123@nate.com>
2020-03-14 00:02:58 +0900

    no duplicate counting method. filter method/trace method is different. + order is important

--- lc_15/lc_15/lc_15_3.cpp
+++ lc_15/lc_15/lc_15_3.cpp
@@ -1,7 +1,25 @@
-// 정렬하여 2중 for + map직접구현
-// 중복이 3개 이상 있는 것은 skip
-// map 직접 구현 (정렬 + 이분검색, 사실 map과는 약간 다름)
-// 추가로 해볼 수 있는게, min/max 가지치기
+/*
+	# 순서
+	기존에 풀던 방식 : 2개 선택 -> 1개 확인
+	새로 풀어볼 방식 : 1개 선택 -> 2개 착기
+
+	동일해 보이지만 확실히 위 둘은 다르다. (순서를 바꿔볼줄 알아야한다.)
+
+	무엇이다른지 모듈을 나눠 생각해보면 보인다.
+	
+		- 두개를 세는 법 (input x일때)
+		1. 2중 for + x와 비교 (후보를 만들어서 filter하는 방식)
+		2. x값을 맞추는 형태의 투 포인터 (입력이 반드시 필요, 값을 찾을 때 입력값에 맞는 값을 추적하는 방식)
+
+		- 한개를 세는 법 (input x일때)
+		1. map (자료구조를 형성한 후 입력에 맞는 값을 추적하는 방식)
+		2. for + x와 비교 (후보를 만들어서 filter하는 방식)
+
+	위를 보면, 한개 세는법과 두개를 세는법의 순서를 무엇을 전후로 두느냐에 따라 조금더 빠를 수 있는 '값을 추적하는 방식'을 쓸 수도 있고 쓸 수 없을 수도 있게 된다.
+
+
+	# 중복 없이 찾는 방법
+*/
 
 
 #define _CRT_SECURE_NO_WARNINGS
@@ -10,120 +28,108 @@
 #include <algorithm>
 #include <unordered_map>
 #include <ctime>
-#include <stdlib.h>
 #define MAIN_DEBUG 1
-#define TCNT 5000
-#define DEBUG 1
-#define NO_VALUE 2000000000
-
 using namespace std;
 
-struct node {
-	int a, b;
-	node(int p, int q) {
-		a = p;
-		b = q;
-	}
-	node() {}
-};
 
 class Solution {
+
 public:
-	int s, e, mid;
-	int search(vector<node>& map, int val) {
-		s = 0;
-		e = map.size() - 1;
-		
-		while (s <= e) {
-			mid = (s + e) / 2;
-			if (map[mid].a > val) e = mid - 1;
-			else if (map[mid].a < val) s = mid + 1;
-			else return map[mid].b;
-		}
-		return NO_VALUE;
-	}
-
-
 	vector<vector<int>> threeSum(vector<int>& nums) {
 		vector<vector<int>> result;
 		vector<int> t;
 		int i, j, k;
-
 		int n = nums.size();
 		if (n == 0) return result;
-
-		int m = 0;
-		int* arr = new int[n];
-		vector<node> map;
 		sort(nums.begin(), nums.end());
-		int zero_cnt = 0;
+
+		int s, e;
 		for (i = 0; i < n; i++) {
-			if (nums[i] == 0) zero_cnt++;
-		}
-
-		if (zero_cnt >= 3) { // 3중복
-			t.push_back(0);
-			t.push_back(0);
-			t.push_back(0);
-			result.push_back(t);
-		}
-
-		int min = nums[0], max = nums[0];
-		int* check = new int[n];
-		arr[0] = nums[0]; map.push_back(node(nums[0], 0));
-		check[0] = 1;
-		for (i = 1; i < n; i++) {
-			if (arr[m] == nums[i]) {
-				if (map.back().a == nums[i]) map.back().b = m;
-				check[m] = 0;
-			}
-			else {
-				arr[++m] = nums[i]; map.push_back(node(nums[i], m));
-				check[m] = 1;
-				if (min > arr[m]) min = arr[m];
-				if (max < arr[m]) max = arr[m];
-			}
-		}
-		m++;
-
-		// 2중복
-		for (i = 0; i < m; i++) {
-			if (check[i] == 0) { // 중복이 있다면, 
-				k = search(map, -(arr[i] + arr[i]));
-				if (arr[i] == 0) continue;
-				if (NO_VALUE != k) {
+			s = i + 1;
+			e = n - 1;
+			while (s < e) {
+				if (nums[s] + nums[e] + nums[i] < 0) s++;
+				else if (nums[s] + nums[e] + nums[i] > 0) e--;
+				else {
 					t.clear();
-					t.push_back(arr[i]);
-					t.push_back(arr[i]);
-					t.push_back(arr[k]);
+					t.push_back(nums[i]);
+					t.push_back(nums[s]);
+					t.push_back(nums[e]);
 					result.push_back(t);
+					while (s < e && nums[s] == t[1]) s++;			// 이미 채택 된 것과 다른 값이 나올때 까지 피벗을 변경
+					
+					//s++;										
+					//while (s < e && nums[s] == nums[s+1]) s++;	// 가장 아래 있는 while문처럼 착각할 수 있는데. 이건 틀리다., 바로 윗줄 s++ 넣어줘도 틀리다. 아예 다르다.
+						
+																	// 자세히 보면, 윗줄은 최소 한번 이상 s++ 해주는데, 아랫줄은 아예 s++을 수행 안할수도 있다. 다른 의미임에 주의하자.
+					//while (s < e && nums[e] == nums[e-1]) e--;		
+
+					while (s < e && nums[e] == t[2]) e--;			// 중복을 피하는 코드 스킬이니까 주의해서 기억해둘 것.
+
 				}
+
 			}
+
+			while (i + 1 < n && nums[i + 1] == nums[i]) i++; // 이걸 for문 바로 밑에 쓰면 process를 한번도 안거치는거고
+															// for문 아래 지금 위치에 쓰면 process를 한번 이상 거치겠다는 것이다.
+															// 두 번 이상진행하지 않으려할때는 이러한 형태의 코드를 쓸 수 있겠다.
 		}
-
-
-
-		// 중복x
-		for (i = 0; i < m; i++) {
-			for (j = i + 1; j < m; j++) {
-				if (-(arr[i] + arr[j]) > max || -(arr[i] + arr[j]) < min) continue;
-				k = search(map, -(arr[i] + arr[j]));
-				if (NO_VALUE == k) continue;
-				
-				if (j < k) {
-					t.clear();
-					t.push_back(arr[i]);
-					t.push_back(arr[j]);
-					t.push_back(arr[k]);
-					result.push_back(t);
-				}
-				else break;
-			}
-		}
-
 		return result;
 	}
+
 };
+
+/*
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <ctime>
+#define MAIN_DEBUG 1
+using namespace std;
+
+
+class Solution {
+
+public:
+	vector<vector<int>> threeSum(vector<int>& nums) {
+		vector<vector<int>> result;
+		vector<int> t;
+		int i, j, k;
+		int n = nums.size();
+		if (n == 0) return result;
+		sort(nums.begin(), nums.end());
+
+		int s, e;
+		for (i = 0; i < n; i++) {
+			s = i + 1;
+			e = n-1;
+			while (s < e) {
+				if (nums[s] + nums[e] + nums[i] < 0) s++;
+				else if (nums[s] + nums[e] + nums[i] > 0) e--;
+				else {
+					t.clear();
+					t.push_back(nums[i]);
+					t.push_back(nums[s]);
+					t.push_back(nums[e]);
+					if (result.empty() || (!result.empty() && result.back() != t)) {
+						result.push_back(t);
+					}
+					s++;
+				}
+			}
+
+			while (i+1 < n && nums[i + 1] == nums[i]) i++;
+		}
+		return result;
+	}
+
+};
+
+*/
 
 
 int main(void) {
@@ -135,10 +141,14 @@ int main(void) {
 	clock_t start = clock();
 
 	vector<int> nums;
-	nums.push_back(-1);
+	nums.push_back(0);
+	nums.push_back(0);
 	nums.push_back(0);
 	nums.push_back(1);
-	nums.push_back(0);
+	nums.push_back(2);
+	nums.push_back(-1);
+//	nums.push_back(4);
+//	nums.push_back(2);
 	ans = sol.threeSum(nums);
 
 #if MAIN_DEBUG==1
